3452848570f283451372090f23749dac6556a511
Qiang Gu <Qiang.Gu@synopsys.com>
2018-01-18 09:50:19 +0800

    travis: update travis script to install lib32

--- .travis/before_install.sh
+++ .travis/before_install.sh
@@ -11,8 +11,10 @@ cd /tmp || die
 
 [ $TRAVIS_OS_NAME != linux ] || {
     sudo apt-get update || die
+    sudo apt-get install lib32z1 || die
+    sudo apt-get install lib32ncurses5 ​|| die
     wget https://github.com/foss-for-synopsys-dwc-arc-processors/toolchain/releases/download/arc-2017.09-release/arc_gnu_2017.09_prebuilt_elf32_le_linux_install.tar.gz || die
-    tar xzvf arc_gnu_2017.09_prebuilt_elf32_le_linux_install.tar.gz || die
+    tar xzf arc_gnu_2017.09_prebuilt_elf32_le_linux_install.tar.gz || die
     export PATH=/tmp/arc_gnu_2017.09_prebuilt_elf32_le_linux_install/bin:$PATH || die
     arc-elf32-gcc --version || die
 }
