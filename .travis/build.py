5691d96032179f8b13119c8c6d88de37f3ff21a7
Qiang Gu <Qiang.Gu@synopsys.com>
2018-02-28 14:31:42 +0800

    travis: Add part of baremetal examples

--- .travis/build.py
+++ .travis/build.py
@@ -4,10 +4,18 @@ import os
 import sys
 
 example = {"arc_feature_cache":"baremetal/arc_feature/cache",
-			"arc_feature_timer_interrupt":"baremetal/arc_feature/timer_interrupt"}
+		"arc_feature_timer_interrupt":"baremetal/arc_feature/timer_interrupt",
+		"arc_feature_udma":"baremetal/arc_feature/udma",
+		"ble_hm1x":"baremetal/ble_hm1x",
+		"blinky":"baremetal/blinky",
+		"bootloader":"baremetal/bootloader"}
 
 result = {"arc_feature_cache":0,
-			"arc_feature_timer_interrupt":0}
+		"arc_feature_timer_interrupt":0,
+		"arc_feature_udma":0,
+		"blinky":0,
+		"bootloader":0}
+
 folder = ".travis"
 
 if __name__ == '__main__':
