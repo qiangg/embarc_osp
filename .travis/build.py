6b9f77ffd95889b7df75e5bf6d33bddd9a18c558
Qiang Gu <Qiang.Gu@synopsys.com>
2018-02-28 16:52:26 +0800

    travis: add freertos examples in travis script
    Signed-off-by: Qiang Gu <Qiang.Gu@synopsys.com>

--- .travis/build.py
+++ .travis/build.py
@@ -9,29 +9,49 @@ example = {"arc_feature_cache":"baremetal/arc_feature/cache",
 		"ble_hm1x":"baremetal/ble_hm1x",
 		"blinky":"baremetal/blinky",
 		"cxx":"baremetal/cxx",
-		"dma_spiflash":"baremetal/dma_spiflash",
 		"graphic_u8glib":"baremetal/graphic_u8glib",
-		"openthread_cli":"baremetal/openthread/cli",
-		"openthread_ncp":"baremetal/openthread/ncp"
+		"iot_coap_coap_server":"freertos/iot/coap/coap_server",
+		"iot_lwm2m_lwm2m_client":"freertos/iot/lwm2m/lwm2m_client",
+		"iot_lwm2m_lwm2m_server":"freertos/iot/lwm2m/lwm2m_server",
+		"kernel":"freertos/kernel",
+		"kernel_secure":"freertos/kernel_secure",
+		"net_httpserver":"freertos/net/httpserver",
+		"net_ntshell":"freertos/net/ntshell",
+		"sec_mbedtls_dtls_client":"freertos/sec/mbedtls/dtls/client",
+		"sec_mbedtls_dtls_server":"freertos/sec/mbedtls/dtls/server",
+		"sec_mbedtls_ssl_client2":"freertos/sec/mbedtls/ssl/client2",
+		"sec_mbedtls_ssl_server2":"freertos/sec/mbedtls/ssl/server2"
 	}
 '''
-"bootloader":"baremetal/bootloader"
-"secureshield_secret_normal":"baremetal/secureshield/secret_normal"
-"secureshield_secret_secure":"baremetal/secureshield/secret_secure"
-"secureshield_secret_secure_sid":"baremetal/secureshield/secret_secure_sid"
-"secureshield_test_case":"baremetal/secureshield/test_case"
+"bootloader":"baremetal/bootloader",
+"dma_spiflash":"baremetal/dma_spiflash",
+"openthread_cli":"baremetal/openthread/cli",
+"openthread_ncp":"baremetal/openthread/ncp",
+"secureshield_secret_normal":"baremetal/secureshield/secret_normal",
+"secureshield_secret_secure":"baremetal/secureshield/secret_secure",
+"secureshield_secret_secure_sid":"baremetal/secureshield/secret_secure_sid",
+"secureshield_test_case":"baremetal/secureshield/test_case",
 
 '''
 
 result = {"arc_feature_cache":0,
 		"arc_feature_timer_interrupt":0,
 		"arc_feature_udma":0,
+		"ble_hm1x":0,
 		"blinky":0,
 		"cxx":0,
-		"dma_spiflash":0,
 		"graphic_u8glib":0,
-		"openthread_cli":0,
-		"openthread_ncp":0
+		"iot_coap_coap_server":0,
+		"iot_lwm2m_lwm2m_client":0,
+		"iot_lwm2m_lwm2m_server":0,
+		"kernel":0,
+		"kernel_secure":0,
+		"net_httpserver":0,
+		"net_ntshell":0,
+		"sec_mbedtls_dtls_client":0,
+		"sec_mbedtls_dtls_server":0,
+		"sec_mbedtls_ssl_client2":0,
+		"sec_mbedtls_ssl_server2":0
 		}
 
 
