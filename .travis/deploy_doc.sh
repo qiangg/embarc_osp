b0448d6248deb73b925fa31fb61b775112b75ee1
Qiang Gu <Qiang.Gu@synopsys.com>
2018-04-26 13:18:36 +0800

    travis: update travis scripts
    Signed-off-by: Qiang Gu <Qiang.Gu@synopsys.com>

--- .travis/deploy_doc.sh
+++ .travis/deploy_doc.sh
@@ -20,7 +20,7 @@ rm -rf embARC_Document.html embARC_Document || die
 tar xzvf doc.tar.gz || die
 rm -rf doc.tar.gz || die
 
-git -diff --quiet
+git diff --quiet
 if [ $? -eq 1 ]; then
 	git add --all || die
 	git commit -s -a -m "Update gh-pages branch" || die
