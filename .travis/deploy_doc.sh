40ca1e81f0909cbfda6a17e0f40261f6af180742
Qiang Gu <Qiang.Gu@synopsys.com>
2018-04-26 16:41:53 +0800

    travis: test travis script
    Signed-off-by: Qiang Gu <Qiang.Gu@synopsys.com>

--- .travis/deploy_doc.sh
+++ .travis/deploy_doc.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+REPO_LINK="https://github.com/qiangg/embarc_osp.git"
+
 die() {
     echo " *** ERROR: " $*
     exit 1
@@ -15,10 +17,18 @@ cd ../doc || die
 make || die
 
 tar czvf doc.tar.gz embARC_Document.html embARC_Document || die
-git fetch origin
-git branch -a
+#git fetch origin
+#git branch -a
+
+mkdir gh-pages || die
+cd gh-pages || die
+git init .
+git remote add origin ${REPO_LINK}
+git fetch origin -t
 git checkout -b gh-pages origin/gh-pages || die
+cd doc
 rm -rf embARC_Document.html embARC_Document || die
+cp ../../doc.tar.gz . || die
 tar xzvf doc.tar.gz || die
 rm -rf doc.tar.gz || die
 
