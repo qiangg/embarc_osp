f3fcaeb0d9ea6ef49195624bf89f258606a31164
Qiang Gu <Qiang.Gu@synopsys.com>
2018-05-10 14:28:26 +0800

    first update for sphinx
    Signed-off-by: Qiang Gu <Qiang.Gu@synopsys.com>

--- doc/embARC_Document/html/search/files_8.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['lwipopts_2eh',['lwipopts.h',['../iot_2coap_2coap__server_2lwipopts_8h.html',1,'']]],
-  ['lwipopts_2eh',['lwipopts.h',['../iot_2lwm2m_2lwm2m__server_2lwipopts_8h.html',1,'']]],
-  ['lwipopts_2eh',['lwipopts.h',['../iot_2lwm2m_2lwm2m__client_2lwipopts_8h.html',1,'']]]
-];
