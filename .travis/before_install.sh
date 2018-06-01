a2ac72bef7f07e2539755b91d454418c52e3872d
Qiang Gu <Qiang.Gu@synopsys.com>
2018-06-01 13:02:18 +0800

    travis: install sphinx_rtd_theme
    Signed-off-by: Qiang Gu <Qiang.Gu@synopsys.com>

--- .travis/before_install.sh
+++ .travis/before_install.sh
@@ -22,5 +22,6 @@ cd /tmp || die
     sudo pip install --upgrade pip || die
     sudo pip install git+https://github.com/sphinx-doc/sphinx || die
     sudo pip install breathe || die
-    sudo pip install recommonmark || die 
+    sudo pip install recommonmark || die
+    sudo pip install sphinx_rtd_theme || die 
 }
